49ba3930338bc6fb2c1b28fe97b576b65ed59f03
vitalyvitmens <otm_@tut.by>
2024-02-13 16:07:39 +0300

    chore(src\App.js): оборачивает роуты в AuthProvider и PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import { AuthProvider } from './context/AuthProvider'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Home, Category, Detail, NotFound } from './pages'
+import { Home, Category, Detail, NotFound, Login } from './pages'
+import { PrivateRoute } from './components'
 import styles from './app.module.css'
 
 export const App = () => {
 	return (
 		<div className={styles.App}>
 			<HashRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/:category" element={<Category />} />
-					<Route path="/:category/:id" element={<Detail />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+				<AuthProvider>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route
+							path="/:category"
+							element={
+								<PrivateRoute>
+									<Category />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="/:category/:id"
+							element={
+								<PrivateRoute>
+									<Detail />
+								</PrivateRoute>
+							}
+						/>
+						<Route path="/login" element={<Login />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</AuthProvider>
 			</HashRouter>
 		</div>
 	)
